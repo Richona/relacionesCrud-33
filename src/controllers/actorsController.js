86cffa44f2c58e9802d0d65ee512b5885ab6935c
Richona <105828822+Richona@users.noreply.github.com>
2022-10-04 15:27:43 -0300

    CR de crud en actors completado

--- src/controllers/actorsController.js
+++ src/controllers/actorsController.js
@@ -15,5 +15,47 @@ module.exports = {
                 res.render("actorsDetail", {actor})
             })
             .catch(error => console.log(error))
+    },
+    add: function (req, res) {
+        db.Movie.findAll()
+            .then(movie => {
+                return res.render("actorsAdd", {movie})
+            })
+    },
+    create: function (req,res) {
+        db.Actor.create({
+            ...req.body
+        })
+            .then(res.redirect("/actors"))
+            .catch(error => console.log(error))
+    },
+    edit: function(req,res) {
+        db.Genre.findByPk(req.params.id)
+            .then(genre => res.render("genresEdit", {genre}))
+            .catch(error => console.log(error))         
+    },
+    update: function (req,res) {
+        db.Genre.update({
+           ...req.body
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(res.redirect("/genres/detail/ "+req.params.id))
+            .catch(error => console.log(error))
+    },
+    delete: function (req,res) {
+        db.Genre.findByPk(req.params.id)
+            .then(genre => res.render("genresDelete",{genre}))
+            .catch(error => console.log(error))
+    },
+    destroy: function (req,res) {
+        db.Genre.destroy({
+            where: {id: req.params.id}
+        })
+            .then(res.redirect("/genres"))
+            .catch(error => console.log(error))
     }
+
 }
--- src/routes/actorsRoutes.js
+++ src/routes/actorsRoutes.js
@@ -5,5 +5,11 @@ const actorsController = require('../controllers/actorsController');
 router.get('/actors', actorsController.list);
 router.get('/actors/detail/:id', actorsController.detail);
 
+router.get('/actors/add', actorsController.add);
+router.post('/actors/create', actorsController.create);
+router.get('/actors/edit/:id', actorsController.edit);
+router.put('/actors/update/:id', actorsController.update);
+router.get('/actors/delete/:id', actorsController.delete);
+router.delete('/actors/delete/:id', actorsController.destroy);
 
 module.exports = router;
