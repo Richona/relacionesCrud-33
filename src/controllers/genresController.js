ac5fffab420c9640c958c41c04f6075e8997fb5f
Richona <105828822+Richona@users.noreply.github.com>
2022-10-04 14:45:21 -0300

    CR de crud en genres completado

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -16,6 +16,54 @@ const genresController = {
             .then(genre => {
                 res.render('genresDetail.ejs', {genre});
             });
+    },
+    add: function (req, res) {
+        return res.render("genresAdd")
+    },
+    create: function (req,res) {
+        db.Genre.create({
+            ...req.body
+        })
+            .then(res.redirect("/genres"))
+            .catch(error => console.log(error))
+    },
+    edit: function(req,res) {
+        const movieToedit = Movies.findByPk(req.params.id)
+
+        const generos = Genres.findAll({order: ["name"]})
+
+        Promise.all([movieToedit,generos])
+            .then(([Movie, allGenres]) => res.render("moviesEdit", {Movie,allGenres, moment}))
+            .catch(error => console.log(error))
+    },
+    update: function (req,res) {
+        const {title, rating, awards, release_date, length, genre_id} = req.body
+        Movies.update({
+            title,
+            rating,
+            awards,
+            release_date,
+            genre_id,
+            length
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(res.redirect("/movies/detail/ "+req.params.id))
+            .catch(error => console.log(error))
+    },
+    delete: function (req,res) {
+        Movies.findByPk(req.params.id)
+            .then(Movie => res.render("moviesDelete",{Movie}))
+            .catch(error => console.log(error))
+    },
+    destroy: function (req,res) {
+        Movies.destroy({
+            where: {id: req.params.id}
+        })
+            .then(res.redirect("/movies"))
+            .catch(error => console.log(error))
     }
 
 }
--- src/routes/genresRoutes.js
+++ src/routes/genresRoutes.js
@@ -5,5 +5,12 @@ const genresController = require('../controllers/genresController');
 router.get('/genres', genresController.list);
 router.get('/genres/detail/:id', genresController.detail);
 
+router.get('/genres/add', genresController.add);
+router.post('/genres/create', genresController.create);
+router.get('/genres/edit/:id', genresController.edit);
+router.put('/genres/update/:id', genresController.update);
+router.get('/genres/delete/:id', genresController.delete);
+router.delete('/genres/delete/:id', genresController.destroy);
+
 
 module.exports = router;
