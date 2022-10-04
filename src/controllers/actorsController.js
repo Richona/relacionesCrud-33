44b05dbed335f20a3d28d9ef1c378bb6318bb3a7
Richona <105828822+Richona@users.noreply.github.com>
2022-10-04 15:40:51 -0300

    U de crud en actors completed

--- src/controllers/actorsController.js
+++ src/controllers/actorsController.js
@@ -17,7 +17,7 @@ module.exports = {
             .catch(error => console.log(error))
     },
     add: function (req, res) {
-        db.Movie.findAll()
+        db.Movie.findAll({order: ["title"]})
             .then(movie => {
                 return res.render("actorsAdd", {movie})
             })
@@ -30,19 +30,23 @@ module.exports = {
             .catch(error => console.log(error))
     },
     edit: function(req,res) {
-        db.Genre.findByPk(req.params.id)
-            .then(genre => res.render("genresEdit", {genre}))
+        const movie = db.Movie.findAll({order: ["title"]});
+
+        const actor = db.Actor.findByPk(req.params.id);
+
+        Promise.all([movie,actor])
+            .then(([movie, actor]) => res.render("actorsEdit", {movie, actor}))
             .catch(error => console.log(error))         
     },
     update: function (req,res) {
-        db.Genre.update({
+        db.Actor.update({
            ...req.body
         }, {
             where: {
                 id: req.params.id
             }
         })
-            .then(res.redirect("/genres/detail/ "+req.params.id))
+            .then(res.redirect("/actors/detail/ "+req.params.id))
             .catch(error => console.log(error))
     },
     delete: function (req,res) {
